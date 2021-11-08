884fa6a83e7ddcc8abefbc4ee794553cf449906a
seredos <seredosvonappen@gmail.com>
2021-11-08 16:11:18 +0100

    take i dont know

--- ci/activation.sh
+++ ci/activation.sh
@@ -1,13 +1,11 @@
-UNITY_VERSION=2020.3.21f1
-IMAGE=unityci/editor # https://hub.docker.com/r/unityci/editor
-IMAGE_VERSION=0.15 # https://github.com/game-ci/docker/releases
-DOCKER_IMAGE=$IMAGE:$UNITY_VERSION-base-$IMAGE_VERSION
+#!/usr/bin/env bash
 
-docker run -it --rm \
--e "UNITY_USERNAME=youremail" \
--e "UNITY_PASSWORD=yourpassword" \
--e "TEST_PLATFORM=linux" \
--e "WORKDIR=/root/project" \
--v "$(pwd):/root/project" \
-$DOCKER_IMAGE \
-bash
+set -e
+set -x
+
+xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' \
+unity-editor \
+-logFile /dev/stdout \
+-batchmode \
+-nographics \
+-username "$UNITY_USERNAME" -password "$UNITY_PASSWORD"
